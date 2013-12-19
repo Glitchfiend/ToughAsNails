063a394113f1060bdcc283fa61169fd9e58b4e80
Adubbz <a.dubbz@hotmail.com>
2013-12-20 10:28:04 +1100

    Added a class for crafting recipes

--- src/main/java/tan/ToughAsNails.java
+++ src/main/java/tan/ToughAsNails.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import tan.configuration.TANConfiguration;
 import tan.core.CreativeTabTAN;
 import tan.core.TANArmour;
+import tan.core.TANCrafting;
 import tan.core.TANItems;
 import tan.core.TANPlayerStats;
 import tan.core.TANPotions;
@@ -26,8 +27,6 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.registry.TickRegistry;
-import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = "ToughAsNails", name = "Tough As Nails", dependencies="required-after:Forge@[1.42.666.42.1,)")
 @NetworkMod(channels = { "ToughAsNails" }, packetHandler = PacketHandler.class, clientSideRequired = true, serverSideRequired = false)
@@ -53,6 +52,7 @@ public class ToughAsNails
         TANPotions.init();
         TANItems.init();
         TANArmour.init();
+        TANCrafting.init();
         TANPlayerStats.init();
         TANTemperature.init();
         TANThirst.init();
---
+++ src/main/java/tan/core/TANCrafting.java
@@ -0,0 +1,20 @@
+package tan.core;
+
+public class TANCrafting
+{
+    public static void init()
+    {
+        addShapedRecipes();
+        addShapelessRecipes();
+    }
+    
+    private static void addShapedRecipes()
+    {
+        
+    }
+    
+    private static void addShapelessRecipes()
+    {
+        
+    }
+}
