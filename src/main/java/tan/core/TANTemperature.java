0fc93f3d76446680297972542bce6ffbd328bee2
Matt Caughey <Catcwe316@comcast.net>
2013-12-23 17:27:51 -0500

    Added other armors as temperature sources

--- src/main/java/tan/core/TANTemperature.java
+++ src/main/java/tan/core/TANTemperature.java
@@ -1,6 +1,7 @@
 package tan.core;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import tan.api.temperature.TemperatureRegistry;
 import tan.eventhandler.modifiers.TemperatureArmourEventHandler;
@@ -41,13 +42,37 @@ public class TANTemperature
     
     private static void registerArmourTemperatureSources()
     {
+    	//Wool
         TemperatureRegistry.registerTemperatureSource("A", TANArmour.helmetWool.itemID, 0, 1.75F, 0.05F);
-        TemperatureRegistry.registerTemperatureSource("A", TANArmour.chestplateWool.itemID, 0, 1.75F, 0.15F);
+        TemperatureRegistry.registerTemperatureSource("A", TANArmour.chestplateWool.itemID, 0, 2F, 0.15F);
         TemperatureRegistry.registerTemperatureSource("A", TANArmour.leggingsWool.itemID, 0, 1.25F, 0.1F);
         TemperatureRegistry.registerTemperatureSource("A", TANArmour.bootsWool.itemID, 0, 0.75F, 0.2F);
         
+        //Leather
+        TemperatureRegistry.registerTemperatureSource("A", Item.helmetLeather.itemID, 0, 0.75F, 0.1F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.plateLeather.itemID, 0, 1F, 0.2F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.legsLeather.itemID, 0, 0.5F, 0.15F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.bootsLeather.itemID, 0, 0.25F, 0.25F);
+        
+        //Gold
+        TemperatureRegistry.registerTemperatureSource("A", Item.helmetGold.itemID, 0, 0.25F, 0.01F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.plateGold.itemID, 0, 0.5F, 0.1F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.legsGold.itemID, 0, 0.25F, 0.05F);
+        
+        //Diamond
+        TemperatureRegistry.registerTemperatureSource("A", Item.helmetDiamond.itemID, 0, -0.5F, 0.1F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.plateDiamond.itemID, 0, -0.5F, 0.2F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.legsDiamond.itemID, 0, -0.25F, 0.15F);
+        
+        //Chainmail
+        TemperatureRegistry.registerTemperatureSource("A", Item.helmetChain.itemID, 0, -0.75F, 0.05F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.plateChain.itemID, 0, -0.75F, 0.15F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.legsChain.itemID, 0, -0.5F, 0.1F);
+        TemperatureRegistry.registerTemperatureSource("A", Item.bootsChain.itemID, 0, -0.5F, 0.2F);
+        
+        //Heat Resistant
         TemperatureRegistry.registerTemperatureSource("A", TANArmour.helmetHeat.itemID, 0, -1.75F, 0.05F);
-        TemperatureRegistry.registerTemperatureSource("A", TANArmour.chestplateHeat.itemID, 0, -1.75F, 0.15F);
+        TemperatureRegistry.registerTemperatureSource("A", TANArmour.chestplateHeat.itemID, 0, -2F, 0.15F);
         TemperatureRegistry.registerTemperatureSource("A", TANArmour.leggingsHeat.itemID, 0, -1.25F, 0.1F);
         TemperatureRegistry.registerTemperatureSource("A", TANArmour.bootsHeat.itemID, 0, -0.75F, 0.2F);
     }
