e3f1cad7aa2faea391d4fd13170ea1a68d6be921
Forstride <forstride@gmail.com>
2016-02-09 06:06:33 -0500

    Proper item registration

--- src/main/java/toughasnails/api/TANItems.java
+++ src/main/java/toughasnails/api/TANItems.java
@@ -6,4 +6,6 @@ public class TANItems
 {
     public static Item canteen;
     public static Item tan_icon;
+    public static Item charcoal_filter;
+    public static Item jelled_slime;
 }
--- src/main/java/toughasnails/init/ModItems.java
+++ src/main/java/toughasnails/init/ModItems.java
@@ -1,9 +1,20 @@
 package toughasnails.init;
 
-import static toughasnails.api.TANItems.canteen;
-import static toughasnails.api.TANItems.tan_icon;
+import java.util.ArrayList;
+import java.util.List;
+
+import static toughasnails.api.TANItems.*;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import toughasnails.core.ToughAsNails;
 import toughasnails.item.ItemCanteen;
 import toughasnails.util.inventory.CreativeTabTAN;
 
@@ -11,18 +22,56 @@ public class ModItems
 {
     public static void init()
     {
+    	registerItems();
+        //setupModels();
+    }
+    
+    public static void registerItems()
+    {
         canteen = registerItem(new ItemCanteen(), "canteen");
         tan_icon = registerItem(new Item(), "tan_icon");
         tan_icon.setCreativeTab(null);
+        charcoal_filter = registerItem(new Item(), "charcoal_filter");
+        jelled_slime = registerItem(new Item(), "jelled_slime");
     }
     
     public static Item registerItem(Item item, String name)
     {
+        return registerItem(item, name, CreativeTabTAN.instance);
+    }
+    
+    public static Item registerItem(Item item, String name, CreativeTabs tab)
+    {    
         item.setUnlocalizedName(name);
-        item.setCreativeTab(CreativeTabTAN.instance);
+        if (tab != null)
+        {
+            item.setCreativeTab(CreativeTabTAN.instance);
+        }
+        GameRegistry.registerItem(item,name);
+        //TANCommand.itemCount++;
         
-        GameRegistry.registerItem(item, name);
+        // register sub types if there are any
+        if (FMLCommonHandler.instance().getSide() == Side.CLIENT)
+        {
+            if (item.getHasSubtypes())
+            {
+                List<ItemStack> subItems = new ArrayList<ItemStack>();
+                item.getSubItems(item, CreativeTabTAN.instance, subItems);
+                for (ItemStack subItem : subItems)
+                {
+                    String subItemName = item.getUnlocalizedName(subItem);
+                    subItemName =  subItemName.substring(subItemName.indexOf(".") + 1); // remove 'item.' from the front
+
+                    ModelBakery.addVariantName(item, ToughAsNails.MOD_ID + ":" + subItemName);
+                    ModelLoader.setCustomModelResourceLocation(item, subItem.getMetadata(), new ModelResourceLocation(ToughAsNails.MOD_ID + ":" + subItemName, "inventory"));
+                }
+            }
+            else
+            {
+                ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(ToughAsNails.MOD_ID + ":" + name, "inventory"));
+            }
+        }
         
-        return item;
+        return item;   
     }
 }
