c3177e46c0478dc518a141caccd6a912aed91dff
Forstride <forstride@gmail.com>
2016-02-19 01:53:31 -0500

    Fixed dispenser behavior, added dungeon loot

--- src/main/java/toughasnails/entities/projectile/DispenserBehaviorTANArrow.java
+++ src/main/java/toughasnails/entities/projectile/DispenserBehaviorTANArrow.java
@@ -1,15 +1,41 @@
 package toughasnails.entities.projectile;
 
+import toughasnails.item.ItemTANArrow;
+import net.minecraft.block.BlockDispenser;
 import net.minecraft.dispenser.BehaviorProjectileDispense;
+import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.dispenser.IPosition;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.IProjectile;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 public class DispenserBehaviorTANArrow extends BehaviorProjectileDispense
 {
 	@Override
-	protected IProjectile getProjectileEntity(World world, IPosition iPosition)
+    public ItemStack dispenseStack(IBlockSource source, ItemStack stack)
+    {
+        World world = source.getWorld();
+        IPosition iposition = BlockDispenser.getDispensePosition(source);
+        EnumFacing enumfacing = BlockDispenser.getFacing(source.getBlockMetadata());
+        IProjectile iprojectile = this.getProjectileEntity(world, iposition, stack);
+        iprojectile.setThrowableHeading((double)enumfacing.getFrontOffsetX(), (double)((float)enumfacing.getFrontOffsetY() + 0.1F), (double)enumfacing.getFrontOffsetZ(), this.func_82500_b(), this.func_82498_a());
+        world.spawnEntityInWorld((Entity)iprojectile);
+        stack.splitStack(1);
+        return stack;
+    }
+	
+	protected IProjectile getProjectileEntity(World world, IPosition iPosition, ItemStack stack)
 	{
-		return new EntityTANArrow(world, iPosition.getX(), iPosition.getY(), iPosition.getZ());
+		EntityTANArrow arrow =  new EntityTANArrow(world, iPosition.getX(), iPosition.getY(), iPosition.getZ());
+		arrow.setArrowType(ItemTANArrow.ArrowType.fromMeta(stack.getMetadata()));
+		return arrow;
+	}
+
+	@Override
+	protected IProjectile getProjectileEntity(World worldIn, IPosition position) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- src/main/java/toughasnails/init/ModVanillaCompat.java
+++ src/main/java/toughasnails/init/ModVanillaCompat.java
@@ -1,18 +1,55 @@
 package toughasnails.init;
 
 import net.minecraft.block.BlockDispenser;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
 import toughasnails.api.TANItems;
 import toughasnails.entities.projectile.DispenserBehaviorTANArrow;
+import toughasnails.item.ItemTANArrow;
 
 public class ModVanillaCompat
 {
     public static void init()
     {
     	registerDispenserBehaviors();
+    	addDungeonLoot();
     }
     
     private static void registerDispenserBehaviors()
     {
     	BlockDispenser.dispenseBehaviorRegistry.putObject(TANItems.arrow, new DispenserBehaviorTANArrow());
     }
+    
+	private static void addDungeonLoot()
+	{
+		ChestGenHooks desertTemple = ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_DESERT_CHEST);
+		ChestGenHooks dungeon = ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST);
+		ChestGenHooks jungleTemple = ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_CHEST);
+		ChestGenHooks mineshaft = ChestGenHooks.getInfo(ChestGenHooks.MINESHAFT_CORRIDOR);
+		ChestGenHooks strongholdCorridor = ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_CORRIDOR);
+		ChestGenHooks strongholdCrossing = ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_CROSSING);
+		ChestGenHooks strongholdLibrary = ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_LIBRARY);
+		ChestGenHooks village = ChestGenHooks.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH);
+		ChestGenHooks bonusChest = ChestGenHooks.getInfo(ChestGenHooks.BONUS_CHEST);
+
+		bonusChest.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.canteen), 1, 1, 15));
+		
+		mineshaft.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.jelled_slime_boots), 1, 1, 5));
+		mineshaft.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.jelled_slime_chestplate), 1, 1, 5));
+		mineshaft.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.jelled_slime_helmet), 1, 1, 5));
+		mineshaft.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.jelled_slime_leggings), 1, 1, 5));
+		mineshaft.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.respirator), 1, 1, 1));
+		mineshaft.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.air_filter), 2, 4, 5));
+		
+		village.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.wool_boots), 1, 1, 5));
+		village.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.wool_helmet), 1, 1, 5));
+		village.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.wool_chestplate), 1, 1, 5));
+		village.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.wool_leggings), 1, 1, 5));
+		
+		dungeon.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.arrow, 1, ItemTANArrow.ArrowType.FIRE_ARROW.ordinal()), 1, 4, 5));
+		dungeon.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.arrow, 1, ItemTANArrow.ArrowType.ICE_ARROW.ordinal()), 1, 4, 7));
+		dungeon.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.arrow, 1, ItemTANArrow.ArrowType.BOMB_ARROW.ordinal()), 1, 4, 3));
+		dungeon.addItem(new WeightedRandomChestContent(new ItemStack(TANItems.arrow, 1, ItemTANArrow.ArrowType.LIGHTNING_ARROW.ordinal()), 1, 4, 1));
+	}
 }
