f56b671a84ae732d0cf333b5f83a69cb696d7583
Forstride <forstride@gmail.com>
2016-03-29 16:31:30 -0400

    Fixed coil bounding box/hardness, added particle effects

--- src/main/java/toughasnails/block/BlockTANTemperatureCoil.java
+++ src/main/java/toughasnails/block/BlockTANTemperatureCoil.java
@@ -7,6 +7,8 @@
  ******************************************************************************/
 package toughasnails.block;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
@@ -17,11 +19,18 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import toughasnails.api.ITANBlock;
+import toughasnails.core.ToughAsNails;
 import toughasnails.item.ItemTANBlock;
+import toughasnails.particle.TANParticleTypes;
 import toughasnails.tileentity.TileEntityTemperatureSpread;
 import toughasnails.util.BlockStateUtils;
 
@@ -29,6 +38,7 @@ public class BlockTANTemperatureCoil extends Block implements ITANBlock, ITileEn
 {
     public static final PropertyEnum<CoilType> VARIANT = PropertyEnum.create("variant", CoilType.class);
     public static final PropertyBool POWERED = PropertyBool.create("powered");
+    protected static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.75D, 1.0D);
     
     // implement ITANBlock
     @Override
@@ -45,15 +55,50 @@ public class BlockTANTemperatureCoil extends Block implements ITANBlock, ITileEn
     
     public BlockTANTemperatureCoil() 
     {
-        super(Material.rock);
-        
+        super(Material.iron);
+        this.setHardness(1.0F);
         this.setDefaultState(this.blockState.getBaseState().withProperty(VARIANT, CoilType.COOLING).withProperty(POWERED, Boolean.valueOf(false)));
+    }
+    
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return BOUNDING_BOX;
+    }
+    
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState state, World world, BlockPos pos)
+    {
+        return BOUNDING_BOX;
     }
     
     @Override
     public IBlockState getStateFromMeta(int meta)
     {
         return this.getDefaultState().withProperty(VARIANT, CoilType.values()[meta & 1]).withProperty(POWERED, Boolean.valueOf((meta & 8) > 0));
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand)
+    {
+    	if (state.getValue(POWERED) == true)
+    	{
+	    	if (state.getValue(VARIANT) == CoilType.HEATING)
+	    	{
+		        double d0 = (double)((float)pos.getX() + 0.4F + rand.nextFloat() * 0.2F);
+		        double d1 = (double)((float)pos.getY() + 0.7F + rand.nextFloat() * 0.3F);
+		        double d2 = (double)((float)pos.getZ() + 0.4F + rand.nextFloat() * 0.2F);
+		        world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+	    	}
+	    	if (state.getValue(VARIANT) == CoilType.COOLING)
+	    	{
+		        double d0 = (double)((float)pos.getX() + 0.4F + rand.nextFloat() * 0.2F);
+		        double d1 = (double)((float)pos.getY() + 0.7F + rand.nextFloat() * 0.3F);
+		        double d2 = (double)((float)pos.getZ() + 0.4F + rand.nextFloat() * 0.2F);
+		        ToughAsNails.proxy.spawnParticle(TANParticleTypes.SNOWFLAKE, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+	    	}
+    	}
     }
     
     @Override
