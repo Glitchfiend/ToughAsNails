2d504021e11e01242c9f7de596f36809e84e0930
Forstride <forstride@gmail.com>
2016-06-30 15:56:32 -0400

    Changed altitude modifier so it gets colder the lower you go instead of hotter

--- src/main/java/toughasnails/temperature/modifier/AltitudeModifier.java
+++ src/main/java/toughasnails/temperature/modifier/AltitudeModifier.java
@@ -35,7 +35,7 @@ public class AltitudeModifier extends TemperatureModifier
         
         if (world.provider.isSurfaceWorld())
         {
-        	newTemperatureLevel += MathHelper.floor_double(((64 - player.posY) / 64) * ALTITUDE_TARGET_MODIFIER) + 1;
+        	newTemperatureLevel -= MathHelper.abs_int(MathHelper.floor_double(((64 - player.posY) / 64) * ALTITUDE_TARGET_MODIFIER) + 1);
         }
         
         debugger.end(newTemperatureLevel);
