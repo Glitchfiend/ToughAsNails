9bdff860c2fe48494457363247d27d6df5d2b1dc
Forstride <forstride@gmail.com>
2016-07-12 14:34:58 -0400

    Fixed version checking

--- src/main/java/toughasnails/core/ToughAsNails.java
+++ src/main/java/toughasnails/core/ToughAsNails.java
@@ -25,11 +25,12 @@ import toughasnails.init.ModPotions;
 import toughasnails.init.ModStats;
 import toughasnails.init.ModVanillaCompat;
 
-@Mod(modid = ToughAsNails.MOD_ID, name = ToughAsNails.MOD_NAME)
+@Mod(modid = ToughAsNails.MOD_ID, version = ToughAsNails.MOD_VERSION, name = ToughAsNails.MOD_NAME)
 public class ToughAsNails
 {
     public static final String MOD_NAME = "Tough As Nails";
     public static final String MOD_ID = "ToughAsNails";
+    public static final String MOD_VERSION = "@MOD_VERSION@";
     
     @Instance(MOD_ID)
     public static ToughAsNails instance;
