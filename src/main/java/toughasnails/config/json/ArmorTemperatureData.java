032043445c9f026eac043a72b4eac0628e7a39fd
CorneliaXaos <cornelia.xaos@gmail.com>
2018-08-12 18:03:16 -0400

    Implemented NBT Tag comparison for Armor Temperature configs
    This configuration allows specifying individual NBT tags for each armor
    piece.  Note that amor piece NBT tags MUST be synchronized with amrmor
    piece names as the index in each list is used for equivalency checking.

--- src/main/java/toughasnails/config/json/ArmorTemperatureData.java
+++ src/main/java/toughasnails/config/json/ArmorTemperatureData.java
@@ -1,8 +1,14 @@
 package toughasnails.config.json;
 
 import com.google.gson.annotations.SerializedName;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
+import net.minecraft.nbt.NBTTagCompound;
+import toughasnails.core.ToughAsNails;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ArmorTemperatureData
@@ -11,10 +17,40 @@ public class ArmorTemperatureData
     public List<String> names;
     @SerializedName("modifier")
     public int modifier;
+    @SerializedName("nbts")
+    public List<String> nbts;
 
-    public ArmorTemperatureData(@Nonnull List<String> names, int modifier)
+    private List<NBTTagCompound> nbtTagCompounds;
+
+    public ArmorTemperatureData(@Nonnull List<String> names, int modifier, @Nonnull List<String> nbts)
     {
         this.names = names;
         this.modifier = modifier;
+        this.nbts = nbts;
+    }
+
+    public ArmorTemperatureData(@Nonnull List<String> names, int modifier) {
+        this(names, modifier, null);
+    }
+
+    public List<NBTTagCompound> getNBTTagCompounds() {
+        if (nbtTagCompounds == null) {
+            ArrayList<NBTTagCompound> compound = new ArrayList<>();
+
+            if (nbts == null) {
+                for (int i = 0; i < 4; i++) compound.add(null);
+            } else {
+                nbts.forEach(nbt -> {
+                    try {
+                        compound.add(JsonToNBT.getTagFromJson(nbt));
+                    } catch (NBTException e) {
+                        ToughAsNails.logger.error("Failed to parse NBT tag for ArmorTemperatureData: adding null NBT.  " +
+                                "This is likely an error. (First armor name: " + names.get(0) + ")");
+                    }
+                });
+            }
+            nbtTagCompounds = Collections.unmodifiableList(compound);
+        }
+        return nbtTagCompounds;
     }
 }
--- src/main/java/toughasnails/temperature/modifier/ArmorModifier.java
+++ src/main/java/toughasnails/temperature/modifier/ArmorModifier.java
@@ -2,9 +2,13 @@ package toughasnails.temperature.modifier;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
+import net.minecraft.nbt.NBTTagCompound;
 import toughasnails.api.temperature.IModifierMonitor;
 import toughasnails.api.temperature.Temperature;
 import toughasnails.init.ModConfig;
+import toughasnails.util.config.NBTUtilExt;
 
 import java.util.stream.IntStream;
 
@@ -22,7 +26,13 @@ public class ArmorModifier extends TemperatureModifier {
         int modifier = IntStream.range(0, 4)
                 .map(i -> ModConfig.armorTemperatureData.stream()
                         .filter(atd -> atd.names.contains(inventory.armorInventory.get(i)
-                        .getItem().getRegistryName().toString()))
+                                .getItem().getRegistryName().toString()))
+                        .filter(atd -> {
+                            int index = atd.names.indexOf(inventory.armorInventory.get(i).getItem().getRegistryName()
+                                    .toString());
+                            NBTTagCompound compare = atd.nbts == null ? null : atd.getNBTTagCompounds().get(index);
+                            return NBTUtilExt.areNBTsEqualOrNull(inventory.armorInventory.get(i).getTagCompound(), compare);
+                        })
                         .findFirst()
                         .map(atd -> atd.modifier).orElse(0)).sum();
         newTemperatureLevel += modifier;
