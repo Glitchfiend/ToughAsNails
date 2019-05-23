97fc96b9cd7f2ccbb03c8dfc7598cf659f7505ff
Timothy Clancy <tim@rockhopper.us>
2019-05-23 00:01:31 -0400

    Made the thermometer item toggle the temperature debug menu when used. Making this configurable is a WIP.

--- src/main/java/toughasnails/item/ItemThermometer.java
+++ src/main/java/toughasnails/item/ItemThermometer.java
@@ -7,17 +7,32 @@
  ******************************************************************************/
 package toughasnails.item;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import toughasnails.api.TANCapabilities;
 import toughasnails.api.temperature.TemperatureHelper;
 import toughasnails.api.temperature.TemperatureScale;
+import toughasnails.temperature.TemperatureDebugger;
+import toughasnails.temperature.TemperatureHandler;
 
 public class ItemThermometer extends Item
 {
@@ -44,4 +59,44 @@ public class ItemThermometer extends Item
             }
         });
     }
+    
+    Map<UUID, Long> messageDebounce = new HashMap<UUID, Long>();
+        
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
+    {
+		if (world.isRemote)
+		{
+			
+			// Prevent spam use of this item.
+			if (!messageDebounce.containsKey(player.getUniqueID()) || (System.currentTimeMillis() - messageDebounce.get(player.getUniqueID()) > 2000))
+			{
+				
+				// Allow the player to toggle the detailed temperature info menu.
+				// TODO: add configuration for allowing this.
+				// TODO: resume testing here.
+				if (true)
+				{			        
+			        Minecraft.getMinecraft().addScheduledTask(() ->
+	                {
+	                    TemperatureHandler temperatureStats = (TemperatureHandler) player.getCapability(TANCapabilities.TEMPERATURE, null);
+	                    TemperatureDebugger debugger = temperatureStats.debugger;
+
+	                    debugger.setGuiVisible(!debugger.isGuiVisible());
+	                });
+				}
+				
+				//BlockPos playerPosition = player.getPosition();
+				//int finalTemperature = TemperatureHandler
+				//		.getTargetTemperatureAt(world, playerPosition);
+
+				//player.addChatMessage(new TextComponentTranslation(
+				//		"item.thermometer.read", finalTemperature));
+				//messageDebounce.put(player.getUniqueID(),
+				//		System.currentTimeMillis());
+			}
+		}
+		
+		return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
+	}
 }
