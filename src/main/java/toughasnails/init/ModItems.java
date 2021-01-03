6a463c3277ed89883ae76641801e77f220c36f1f
Forstride <forstride@gmail.com>
2021-01-03 00:09:06 -0500

    Added additional canteens

--- src/main/java/toughasnails/init/ModItems.java
+++ src/main/java/toughasnails/init/ModItems.java
@@ -26,9 +26,11 @@ public class ModItems
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
-        dirty_water_bottle = registerItem(new DirtyWaterBottleItem((new Item.Properties()).stacksTo(1).tab(ItemGroupTAN.INSTANCE)), "dirty_water_bottle");
         empty_canteen = registerItem(new EmptyCanteenItem((new Item.Properties()).stacksTo(1).tab(ItemGroupTAN.INSTANCE)), "empty_canteen");
+        dirty_water_canteen = registerItem(new FilledCanteenItem((new Item.Properties()).durability(5).tab(ItemGroupTAN.INSTANCE)), "dirty_water_canteen");
         water_canteen = registerItem(new FilledCanteenItem((new Item.Properties()).durability(5).tab(ItemGroupTAN.INSTANCE)), "water_canteen");
+        purified_water_canteen = registerItem(new FilledCanteenItem((new Item.Properties()).durability(5).tab(ItemGroupTAN.INSTANCE)), "purified_water_canteen");
+        dirty_water_bottle = registerItem(new DirtyWaterBottleItem((new Item.Properties()).stacksTo(1).tab(ItemGroupTAN.INSTANCE)), "dirty_water_bottle");
         purified_water_bottle = registerItem(new PurifiedWaterBottleItem((new Item.Properties()).stacksTo(1).tab(ItemGroupTAN.INSTANCE)), "purified_water_bottle");
 
         tan_icon = registerItem(new Item(new Item.Properties()), "tan_icon");
