3cb0234a2ace34e16261ca7c3df15e9dbff67fab
Adubbz <Adubbz@users.noreply.github.com>
2021-02-05 19:58:40 +1100

    Repurposed repo

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center"><img src="https://i.imgur.com/uNgjobq.png"></p>
+
+**Tough As Nails** is a **Minecraft mod** that adds features to enhance survival mode and increase the game's difficulty.
+
+This repository contains the mod's API and language files. Issues can also be reported on the issue tracker.
+
+-----------------
+
+Tough As Nails is All Rights Reserved.
---
+++ src/main/java/toughasnails/api/thirst/WaterType.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright 2021, the Glitchfiend Team.
+ *
+ * All Rights Reserved.
+ ******************************************************************************/
+package toughasnails.api.thirst;
+
+public enum WaterType
+{
+    NORMAL(0.5F), DIRTY(1.0F), PURIFIED(0.0F);
+
+    private float poisonChance;
+
+    WaterType(float poisonChance)
+    {
+        this.poisonChance = poisonChance;
+    }
+
+    public float getPoisonChance()
+    {
+        return this.poisonChance;
+    }
+}
