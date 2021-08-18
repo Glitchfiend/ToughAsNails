ce17930d24d3d1bbd5afee15cf48f5543cd48d81
Adubbz <Adubbz@users.noreply.github.com>
2021-08-18 21:10:57 +1000

    Ported all mixins to ASM

---
+++ src/main/java/toughasnails/thirst/ThirstHooks.java
@@ -0,0 +1,127 @@
+package toughasnails.thirst;
+
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
+import net.minecraft.world.level.GameRules;
+import toughasnails.api.thirst.IThirst;
+import toughasnails.api.thirst.ThirstHelper;
+import toughasnails.config.ServerConfig;
+
+public class ThirstHooks
+{
+    /*
+       Hooks called by ASM
+    */
+    public static void onCauseFoodExhaustion(Player player, float exhaustion)
+    {
+        if (ServerConfig.enableThirst.get())
+        {
+            ThirstHelper.getThirst(player).addExhaustion(exhaustion);
+        }
+    }
+
+    public static void doFoodDataTick(FoodData data, Player player)
+    {
+        Difficulty difficulty = player.level.getDifficulty();
+        IThirst thirst = ThirstHelper.getThirst(player);
+
+        data.lastFoodLevel = data.foodLevel;
+
+        if (data.exhaustionLevel > 4.0F)
+        {
+            data.exhaustionLevel -= 4.0F;
+
+            if (data.saturationLevel > 0.0F)
+            {
+                data.saturationLevel = Math.max(data.saturationLevel - 1.0F, 0.0F);
+            }
+            else if (difficulty != Difficulty.PEACEFUL)
+            {
+                data.foodLevel = Math.max(data.foodLevel - 1, 0);
+            }
+        }
+
+        boolean naturalRegen = player.level.getGameRules().getBoolean(GameRules.RULE_NATURAL_REGENERATION);
+
+        if (naturalRegen && data.saturationLevel > 0.0F && thirst.getHydration() > 0.0F && player.isHurt() && data.foodLevel >= 20 && thirst.getThirst() >= 20)
+        {
+            ++data.tickTimer;
+
+            if (data.tickTimer >= 10)
+            {
+                // Average the saturation and hydration
+                float replenishAmount = (Math.min(data.saturationLevel, 6.0F) + Math.min(thirst.getHydration(), 6.0F)) / 2.0F;
+
+                player.heal(replenishAmount / 6.0F);
+                data.addExhaustion(replenishAmount);
+                thirst.addExhaustion(replenishAmount);
+                data.tickTimer = 0;
+            }
+        }
+        else if (naturalRegen && data.foodLevel >= 18 && thirst.getThirst() >= 18 && player.isHurt())
+        {
+            ++data.tickTimer;
+            if (data.tickTimer >= 80)
+            {
+                player.heal(1.0F);
+                data.addExhaustion(6.0F);
+                thirst.addExhaustion(6.0F);
+                data.tickTimer = 0;
+            }
+        }
+        else if (data.foodLevel <= 0)
+        {
+            ++data.tickTimer;
+            if (data.tickTimer >= 80)
+            {
+                if (player.getHealth() > 10.0F || difficulty == Difficulty.HARD || player.getHealth() > 1.0F && difficulty == Difficulty.NORMAL)
+                {
+                    player.hurt(DamageSource.STARVE, 1.0F);
+                }
+
+                data.tickTimer = 0;
+            }
+        }
+        else
+        {
+            data.tickTimer = 0;
+        }
+    }
+
+    public static void onAiStep(LocalPlayer player)
+    {
+        if (player.isSprinting())
+        {
+            boolean sprintingAllowable = canSprintWithThirst(player);
+
+            if (player.isSwimming())
+            {
+                if (!player.isOnGround() && !player.input.shiftKeyDown && !sprintingAllowable || !player.isInWater())
+                {
+                    player.setSprinting(false);
+                }
+            }
+            else if (!sprintingAllowable)
+            {
+                player.setSprinting(false);
+            }
+        }
+    }
+
+    public static void onAiStepSetSprinting(LocalPlayer player, boolean sprinting)
+    {
+        // Don't allow sprinting if the player has insufficient thirst
+        if (sprinting && !canSprintWithThirst(player))
+            sprinting = false;
+
+        player.setSprinting(sprinting);
+    }
+
+    private static boolean canSprintWithThirst(LocalPlayer player)
+    {
+        return ThirstHelper.getThirst(player).getThirst() > 6 || player.getAbilities().mayfly;
+    }
+}
---
+++ src/main/resources/transformers/thirst.js
@@ -0,0 +1,152 @@
+/*******************************************************************************
+ * Copyright 2021, the Glitchfiend Team.
+ * All rights reserved.
+ ******************************************************************************/
+ var ASM = Java.type("net.minecraftforge.coremod.api.ASMAPI");
+
+ var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+ var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+ var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+ var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+ 
+var SET_SPRINTING = ASM.mapMethod("m_6858_");
+
+ function Transformation(name, desc) {
+     this.name = name;
+     this.desc = desc;
+     this.funcs = [];
+ 
+     for (var i = 2; i < arguments.length; i++) {
+         this.funcs.push(arguments[i]);
+     }
+ }
+ 
+ var TRANSFORMATIONS = {
+    "net/minecraft/world/entity/player/Player": [
+        new Transformation(ASM.mapMethod("m_36399_"), "(F)V", patchCauseFoodExhaustion) //causeFoodExhaustion
+    ],
+    "net/minecraft/world/food/FoodData": [
+        new Transformation(ASM.mapMethod("m_38710_"), "(Lnet/minecraft/world/entity/player/Player;)V", patchFoodDataTick) //tick
+    ],
+    "net/minecraft/client/player/LocalPlayer": [
+        new Transformation(ASM.mapMethod("m_8107_"), "()V", patchLocalPlayerAiStep) //aiStep
+    ]
+ };
+ 
+ function applyTransformations(classNode, method) {
+     if (!(classNode.name in TRANSFORMATIONS)) {
+         return;
+     }
+ 
+     var transformations = TRANSFORMATIONS[classNode.name];
+ 
+     for (var i = 0; i < transformations.length; i++) {
+         var transformation = transformations[i];
+ 
+         if (transformation.name == method.name && transformation.desc == method.desc) {
+             log("Transforming " + method.name + " " + method.desc + " in " + classNode.name);
+ 
+             for each (var func in transformation.funcs) {
+                 func(method);
+             }
+             break;
+         }
+     }
+ }
+ 
+ function log(message) {
+     print("[Tough As Nails Transformer]: " + message);
+ }
+ 
+ function initializeCoreMod() {
+     return {
+         "thirst_transformer": {
+             "target": {
+                 "type": "CLASS",
+                 "names": function(listofclasses) { return Object.keys(TRANSFORMATIONS); }
+             },
+             "transformer": function(classNode) {
+                 for each (var method in classNode.methods) {
+                     applyTransformations(classNode, method);
+                 }
+ 
+                 return classNode;
+             }
+         }
+     };
+ }
+
+ function patchCauseFoodExhaustion(node) {
+    var insns = new InsnList();
+    insns.add(new VarInsnNode(Opcodes.ALOAD, 0));
+    insns.add(new VarInsnNode(Opcodes.FLOAD, 1));
+    insns.add(ASM.buildMethodCall(
+        "toughasnails/thirst/ThirstHooks",
+        "onCauseFoodExhaustion",
+        "(Lnet/minecraft/world/entity/player/Player;F)V",
+        ASM.MethodType.STATIC
+    ));
+    node.instructions.insertBefore(node.instructions.getFirst(), insns);
+    log("Successfully patched causeFoodExhaustion");
+ }
+
+ function patchFoodDataTick(node) {
+    var insns = new InsnList();
+    insns.add(new VarInsnNode(Opcodes.ALOAD, 0));
+    insns.add(new VarInsnNode(Opcodes.ALOAD, 1));
+    insns.add(ASM.buildMethodCall(
+        "toughasnails/thirst/ThirstHooks",
+        "doFoodDataTick",
+        "(Lnet/minecraft/world/food/FoodData;Lnet/minecraft/world/entity/player/Player;)V",
+        ASM.MethodType.STATIC
+    ));
+    insns.add(new InsnNode(Opcodes.RETURN));
+    node.instructions.insertBefore(node.instructions.getFirst(), insns);
+    log("Successfully patched tick");
+ }
+
+ function patchLocalPlayerAiStep(node) {
+    var insns = new InsnList();
+    insns.add(new VarInsnNode(Opcodes.ALOAD, 0));
+    insns.add(ASM.buildMethodCall(
+        "toughasnails/thirst/ThirstHooks",
+        "onAiStep",
+        "(Lnet/minecraft/client/player/LocalPlayer;)V",
+        ASM.MethodType.STATIC
+    ));
+    node.instructions.insertBefore(node.instructions.getFirst(), insns);
+    log("Successfully inserted hook into aiStep");
+
+    var startIndex = 0;
+    var patchedCount = 0;
+
+    // Redirect all calls to setSprinting to us
+    while (true) {
+        var call = ASM.findFirstMethodCallAfter(node,
+            ASM.MethodType.VIRTUAL,
+            "net/minecraft/client/player/LocalPlayer",
+            SET_SPRINTING,
+            "(Z)V",
+            startIndex);
+
+        if (call == null) {
+            break;
+        }
+
+        startIndex = node.instructions.indexOf(call);
+        node.instructions.insertBefore(call, ASM.buildMethodCall(
+            "toughasnails/thirst/ThirstHooks",
+            "onAiStepSetSprinting",
+            "(Lnet/minecraft/client/player/LocalPlayer;Z)V",
+            ASM.MethodType.STATIC
+        ));
+        node.instructions.remove(call);
+        patchedCount++;
+    }
+
+    if (patchedCount == 0) {
+        log('Failed to locate call to setSprinting in ' + method.name);
+    } else {
+        log('Patched ' + patchedCount + ' calls');
+    }
+ }
