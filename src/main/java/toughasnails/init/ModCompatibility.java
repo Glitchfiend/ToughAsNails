83aa52797495b91da79c4e3cb938b05baebfe94e
Adubbz <Adubbz@users.noreply.github.com>
2021-08-22 09:15:31 +1000

    Don't change the seasonal temperature in biomes that use tropical seasons

--- src/main/java/toughasnails/init/ModCompatibility.java
+++ src/main/java/toughasnails/init/ModCompatibility.java
@@ -5,8 +5,11 @@
 package toughasnails.init;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.fml.ModList;
+import sereneseasons.config.BiomeConfig;
 import toughasnails.api.temperature.TemperatureHelper;
 import toughasnails.api.temperature.TemperatureLevel;
 
@@ -27,6 +30,12 @@ public class ModCompatibility
 
     private static TemperatureLevel seasonModifier(Level level, BlockPos pos, TemperatureLevel current)
     {
+        ResourceKey<Biome> biomeKey = level.getBiomeName(pos).orElse(null);
+
+        // Don't adjust the season if tropical seasons are in use
+        if (BiomeConfig.usesTropicalSeasons(biomeKey))
+            return current;
+
         Season season = SeasonHelper.getSeasonState(level).getSeason();
 
         switch (season)
