62bac437f51225fcc6e317e7d77b8b22aaa833d9
tontaiconmemay <104762155+tontaiconmemay@users.noreply.github.com>
2022-05-02 23:20:33 +0700

    Update TANEffects.java

--- src/main/java/toughasnails/api/potion/TANEffects.java
+++ src/main/java/toughasnails/api/potion/TANEffects.java
@@ -4,12 +4,13 @@
  ******************************************************************************/
 package toughasnails.api.potion;
 
+import effect.minecraft.weakness;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder("toughasnails")
 public class TANEffects
 {
-    public static final MobEffect THIRST = null;
+    public static final MobEffect THIRST = "weakness";
     public static final MobEffect ICE_RESISTANCE = null;
 }
