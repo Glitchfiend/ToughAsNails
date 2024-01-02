6b9c6139e6775fbc18e5968543cc9927825f8cc5
Adubbz <Adubbz@users.noreply.github.com>
2024-01-02 20:43:24 +1100

    Use getEntityRepresentation for parity with Vanilla

--- common/src/main/java/toughasnails/init/ModItems.java
+++ common/src/main/java/toughasnails/init/ModItems.java
@@ -94,7 +94,7 @@ public class ModItems
             @Override
             public float unclampedCall(ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity entity, int seed)
             {
-                Entity holder = entity != null ? entity : stack.getFrame();
+                Entity holder = entity != null ? entity : stack.getEntityRepresentation();
 
                 if (holder == null)
                     return 0.5F;
