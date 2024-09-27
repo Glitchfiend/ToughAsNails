db086426eeb7b304e1570e1355a4ad67cffe32ce
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-09-27 09:31:01 +0200

    make config more user frienldy

--- common/src/main/java/toughasnails/config/ClientConfigModel.java
+++ common/src/main/java/toughasnails/config/ClientConfigModel.java
@@ -1,5 +1,6 @@
 package toughasnails.config;
 
+import blue.endless.jankson.Comment;
 import io.wispforest.owo.config.Option.SyncMode;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
@@ -8,11 +9,15 @@ import toughasnails.api.TANAPI;
 
 @Sync(SyncMode.NONE)
 @Modmenu(modId = TANAPI.MOD_ID)
-@Config(name = "TAN_clientconfig", wrapperName = "ClientConfig")
+@Config(name = "ToughAsNails_clientconfig", wrapperName = "ClientConfig")
 public class ClientConfigModel {
   // Gui
+  @Comment("The offset of the left of the thirst overlay from its default position.")
   public int thirstLeftOffset = 0;
+  @Comment("The offset of the top of the thirst overlay from its default position.")
   public int thirstTopOffset = 0;
+  @Comment("The offset of the left of the temperature overlay from its default position.")
   public int temperatureLeftOffset = 0;
+  @Comment("The offset of the top of the temperature overlay from its default position.")
   public int temperatureTopOffset = 0;
 }
--- common/src/main/java/toughasnails/config/ThirstConfigModel.java
+++ common/src/main/java/toughasnails/config/ThirstConfigModel.java
@@ -1,22 +1,31 @@
 package toughasnails.config;
 
+import blue.endless.jankson.Comment;
 import io.wispforest.owo.config.Option.SyncMode;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.RangeConstraint;
 import io.wispforest.owo.config.annotation.Sync;
 
 @Sync(SyncMode.OVERRIDE_CLIENT)
-@Config(name = "TAN_thirst", wrapperName = "ThirstConfig")
+@Config(name = "ToughAsNails_thirst", wrapperName = "ThirstConfig")
 public class ThirstConfigModel {
+  @Comment("Enable or disable thirst.")
   public boolean enableThirst = true;
-  public boolean enableHandDrinking = false;
+  @Comment("Enable or disable hand drinking.")
+  public boolean enableHandDrinking = true;
+  @Comment("Remove source blocks when filling the canteen.")
   public boolean removeSourceBlocks = false;
+  @Comment("Prevent sprinting when thirsty.")
   public boolean thirstPreventSprint = true;
+  @Comment("Prevent health regeneration when thirsty.")
   public boolean thirstPreventHealthRegen = true;
+  @Comment("The threshold at which exhaustion causes a reduction in hydration and the thirst bar.")
   @RangeConstraint(min = 0.0D, max = Double.MAX_VALUE)
   public double thirstExhaustionThreshold = 8.0D;
+  @Comment("Thirst restored from drinking with hands.")
   @RangeConstraint(min = 0, max = 20)
   public int handDrinkingThirst = 1;
+  @Comment("Hydration restored from drinking with hands.")
   @RangeConstraint(min = 0.0D, max = Double.MAX_VALUE)
   public double handDrinkingHydration = 0.1D;
 }
