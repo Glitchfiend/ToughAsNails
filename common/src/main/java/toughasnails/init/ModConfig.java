5354616a6e64d7b7f42b277267a5901f2142f699
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-09-27 17:51:43 +0200

    update glitchcore and use configsync

--- common/src/main/java/toughasnails/init/ModConfig.java
+++ common/src/main/java/toughasnails/init/ModConfig.java
@@ -4,6 +4,7 @@
  ******************************************************************************/
 package toughasnails.init;
 
+import glitchcore.config.ConfigSync;
 import glitchcore.util.Environment;
 import toughasnails.config.ClientConfig;
 import toughasnails.config.TemperatureConfig;
@@ -19,7 +20,9 @@ public class ModConfig
     {
         thirst = new ThirstConfig();
         temperature = new TemperatureConfig();
-        // sync them now
+
+        ConfigSync.register(thirst);
+        ConfigSync.register(temperature);
 
         if (Environment.isClient())
         {
