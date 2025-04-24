0d61cd83ff16a5453833e5a45948ae993e285c48
Adubbz <Adubbz@users.noreply.github.com>
2025-04-24 11:18:27 +1000

    Fixed server crash on Fabric. Closes #909

--- common/src/main/java/toughasnails/mixin/client/MixinItemStack.java
+++ common/src/main/java/toughasnails/mixin/client/MixinItemStack.java
@@ -2,7 +2,7 @@
  * Copyright 2024, the Glitchfiend Team.
  * All rights reserved.
  ******************************************************************************/
-package toughasnails.mixin;
+package toughasnails.mixin.client;
 
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.component.DataComponentType;
@@ -13,7 +13,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.DyedItemColor;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
