002550932ba4885a238c53d63db1b6200b60426c
SkuldNorniern <skuldnorniern@gmail.com>
2026-07-02 01:32:43 +0900

    fix: restore 1.21.11 armor color behavior - remove DYED_COLOR from leaf armor

--- common/src/main/java/toughasnails/init/ModItems.java
+++ common/src/main/java/toughasnails/init/ModItems.java
@@ -40,15 +40,15 @@ public class ModItems
         // Items
         TANItems.THERMOMETER = registerItem(func, "thermometer", Item::new, new Item.Properties().stacksTo(1));
 
-        TANItems.LEAF_HELMET = registerItem(func, "leaf_helmet", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.LEAF, ArmorType.HELMET).stacksTo(1));
-        TANItems.LEAF_CHESTPLATE = registerItem(func, "leaf_chestplate", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.LEAF, ArmorType.CHESTPLATE).stacksTo(1));
-        TANItems.LEAF_LEGGINGS = registerItem(func, "leaf_leggings", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.LEAF, ArmorType.LEGGINGS).stacksTo(1));
-        TANItems.LEAF_BOOTS = registerItem(func, "leaf_boots", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.LEAF, ArmorType.BOOTS).stacksTo(1));
+        TANItems.LEAF_HELMET = registerItem(func, "leaf_helmet", new Item.Properties().humanoidArmor(ModArmorMaterials.LEAF, ArmorType.HELMET).stacksTo(1));
+        TANItems.LEAF_CHESTPLATE = registerItem(func, "leaf_chestplate", new Item.Properties().humanoidArmor(ModArmorMaterials.LEAF, ArmorType.CHESTPLATE).stacksTo(1));
+        TANItems.LEAF_LEGGINGS = registerItem(func, "leaf_leggings", new Item.Properties().humanoidArmor(ModArmorMaterials.LEAF, ArmorType.LEGGINGS).stacksTo(1));
+        TANItems.LEAF_BOOTS = registerItem(func, "leaf_boots", new Item.Properties().humanoidArmor(ModArmorMaterials.LEAF, ArmorType.BOOTS).stacksTo(1));
 
-        TANItems.WOOL_HELMET = registerItem(func, "wool_helmet", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.HELMET).stacksTo(1));
-        TANItems.WOOL_CHESTPLATE = registerItem(func, "wool_chestplate", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.CHESTPLATE).stacksTo(1));
-        TANItems.WOOL_LEGGINGS = registerItem(func, "wool_leggings", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.LEGGINGS).stacksTo(1));
-        TANItems.WOOL_BOOTS = registerItem(func, "wool_boots", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(DyedItemColor.LEATHER_COLOR)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.BOOTS).stacksTo(1));
+        TANItems.WOOL_HELMET = registerItem(func, "wool_helmet", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(0xFFFFFF)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.HELMET).stacksTo(1));
+        TANItems.WOOL_CHESTPLATE = registerItem(func, "wool_chestplate", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(0xFFFFFF)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.CHESTPLATE).stacksTo(1));
+        TANItems.WOOL_LEGGINGS = registerItem(func, "wool_leggings", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(0xFFFFFF)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.LEGGINGS).stacksTo(1));
+        TANItems.WOOL_BOOTS = registerItem(func, "wool_boots", new Item.Properties().component(DataComponents.DYED_COLOR, new DyedItemColor(0xFFFFFF)).humanoidArmor(ModArmorMaterials.WOOL, ArmorType.BOOTS).stacksTo(1));
 
         TANItems.ICE_CREAM = registerItem(func, "ice_cream", StackableBowlFoodItem::new, new Item.Properties().stacksTo(16).component(DataComponents.CONSUMABLE, Consumables.DEFAULT_FOOD).usingConvertsTo(Items.BOWL).food(new FoodProperties.Builder().nutrition(0).saturationModifier(0.0F).alwaysEdible().build()));
         TANItems.CHARC_0S = registerItem(func, "charc_os", StackableBowlFoodItem::new, new Item.Properties().stacksTo(16).component(DataComponents.CONSUMABLE, Consumables.DEFAULT_FOOD).usingConvertsTo(Items.BOWL).food(new FoodProperties.Builder().nutrition(0).saturationModifier(0.0F).alwaysEdible().build()));
